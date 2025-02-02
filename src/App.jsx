85b8d99aa2575a84da75926ec0726d8de5255541
Jeriel Martinez <martinez.jeriel01@gmail.com>
2025-02-02 13:27:14 -0500

    Remove lazy loading for routes in App component for improved performance and simplicity

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from 'react';
+import Home from "./pages/Home";
+import Library from "./pages/Library";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
-
-// Lazy load routes
-const Home = lazy(() => import('./pages/Home'));
-const Library = lazy(() => import('./pages/Library'));
-const Search = lazy(() => import('./pages/Search'));
-const Profile = lazy(() => import('./pages/Profile'));
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        <Suspense fallback={<div className="loading">Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
         <Navbar />
       </div>
     </Router>
